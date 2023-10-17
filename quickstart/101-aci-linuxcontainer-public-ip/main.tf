11df759ba9a75e0577d5d5fb0c43f4291d54cdd5
lili4020 <118247866+lili4020@users.noreply.github.com>
2023-10-17 11:09:47 +0200

    Update main.tf

--- quickstart/101-aci-linuxcontainer-public-ip/main.tf
+++ quickstart/101-aci-linuxcontainer-public-ip/main.tf
@@ -14,30 +14,50 @@ resource "random_string" "container_name" {
   special = false
 }
 
-resource "azurerm_container_group" "example" {
-  name                = "example-container-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  os_type             = "Linux"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.virtual_network_name_prefix}-${random_string.container_name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"] # Replace with your desired address space
 
-  ip_address_type = "public"
-  
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24" # Replace with your desired subnet address prefix
+  }
+}
+
+resource "azurerm_container_group" "container" {
+  name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_address_type     = "Public"
+  os_type             = "Linux"
+  restart_policy      = var.restart_policy
+
   container {
-    name   = "example-container"
-    image  = "nginx"
-    cpu    = "0.5"
-    memory = "1.5"
+    name   = "${var.container_name_prefix}-${random_string.container_name.result}"
+    image  = var.image
+    cpu    = var.cpu_cores
+    memory = var.memory_in_gb
+
     ports {
-      port     = 80
+      port     = var.port
       protocol = "TCP"
     }
   }
 
-  tags = {
-    environment = "Production"
-  }
-
   network_profile {
     network_plugin = "azure"
+    network_policy = "calico"
+    dns_config {
+      name_servers = ["10.0.0.4"] # Replace with your DNS server address
+    }
+
+    ip_configuration {
+      name                 = "internal"
+      subnet_id            = azurerm_virtual_network.vnet.subnet.id
+      public_ip_address_id = null
+    }
   }
 }
+
